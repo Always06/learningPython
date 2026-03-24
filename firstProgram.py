9e6246ac2e48a600cc92a200f66ebea721a4897a
Always06 <Ganhaiyue10@gmail.com>
2026-03-23 21:45:37 -0400

    Finished first program
    Finish the first runnable program in python.

---
+++ firstProgram.py
@@ -0,0 +1,16 @@
+import sys
+# To check the Python version of the editor.
+
+print(sys.version) 
+# To print out the python version.
+
+print('Hello World!')
+# Print function will print out what's in it's brackets.
+
+# Python is easiler to learn.
+# Python can be used to handle big data and perform complex mathematics.
+# Python can be used on a server to create web applications.
+# Python works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc).
+
+# Download python, learn how to navigate though teriminal. 
+# Created my first program and printed out 1. python version 2. 'Hello World!'.
