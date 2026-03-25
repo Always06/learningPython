40d9fab380000c7dcb51737a624ecbaaf8fdee80
Always06 <Ganhaiyue10@gmail.com>
2026-03-24 23:53:23 -0400

    Python Syntax
    create a second program to write the role of python syntax.

---
+++ secondProgram.py
@@ -0,0 +1,39 @@
+## Statements
+# A computer program is a list of "instructions" to be "executed" by a computer.
+# In a programming language, these programming instructions are called statements.
+# In Python, a statement usually ends when the line ends. You do not need to use a semicolon (;).
+# The statements are executed one by one, in the same order as they are written.
+## examples: 
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+
+## Semicolons
+# Semicolons are optional in Python. You can write multiple statements on one line by separating them with ; 
+# But this is rarely used because it makes it hard to read.
+# However, if you put two statements on the same line without a separator (newline or ;), Python will give an error.
+## examples:
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+
+## Syntax
+# Python's indentation is important because without the indent the code will have syntax errors.
+# Python uses indentation to indicate a block of code.
+# Indentation in Python is very important.
+# The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
+# You have to use the same number of spaces in the same block of code, otherwise Python will give you an error. 
+## example: 
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+
+## variable 
+# A variable is created when you assign a value to it.
+## example:
+x = 5
+y = "Hello, World!"
+print(x)
+print(y)
