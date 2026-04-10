1af504158c09ae98a5642d8da2d6108d9b43a805
Always06 <Ganhaiyue10@gmail.com>
2026-04-09 23:39:45 -0400

    create sixthProgram.py
    In sixth program, it's about how to name variable.

---
+++ sixthProgram.py
@@ -0,0 +1,40 @@
+## Python Variable Names
+# A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume).
+# Rules for Python variables:
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# A variable name cannot be any of the Python keywords.
+## Examples:
+# Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+# Illegal variable names:
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
+# Remember that variable names are case-sensitive
+
+## Python Multi Words Variable Names
+# Variable names with more than one word can be difficult to read.
+# There are several techniques you can use to make them more readable:
+
+## Python Camel Case
+# Each word, except the first, starts with a capital letter:
+## Examples:
+myVariableName = "John"
+
+## Python Pascal Case
+# Each word starts with a capital letter:
+## Examples:
+MyVariableName = "John"
+
+## Python Snake Case
+# Each word is separated by an underscore character:
+## Examples:
+my_variable_name = "John"
