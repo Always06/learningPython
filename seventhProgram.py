2ef18a6e9697c5d463cfcc55dc90ac287c74d392
Always06 <Ganhaiyue10@gmail.com>
2026-04-09 23:43:40 -0400

    Create seventhProgram.py
    In seventh program, show how to assign multiple variables.

---
+++ seventhProgram.py
@@ -0,0 +1,29 @@
+## Python Variables - Assign Multiple Values
+# Many Values to Multiple Variables
+# Python allows you to assign values to multiple variables in one line:
+## Examples:
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+# Note: Make sure the number of variables matches the number of values 
+# Or else you will get an error.
+
+## Python One Value to Multiple Variables
+# And you can assign the same value to multiple variables in one line:
+## Examples:
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+## Python Unpack a Collection
+# If you have a collection of values in a list, tuple etc. 
+# Python allows you to extract the values into variables. This is called unpacking.
+## Examples:
+# Unpack a list:
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
